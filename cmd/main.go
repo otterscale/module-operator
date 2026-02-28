7f31c7f6628877d874f45c05669607deebedcdb1
Chung-Hsuan Tsai <paul_tsai@phison.com>
2026-02-28 15:57:23 +0800

    fix: update LeaderElectionID to reflect new project name in main.go (#15)

--- cmd/main.go
+++ cmd/main.go
@@ -167,7 +167,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "otterscale-template-leader-election",
+		LeaderElectionID:       "addons-operator-leader-election",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
